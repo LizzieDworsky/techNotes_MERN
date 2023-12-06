ebd0b3529eaa961682a34e592906a58a3c722e98
LizzieDworsky <amydworsky15@gmail.com>
2023-12-06 16:16:34 -0700

    set up middleware error handling + adding additional middleware

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 3500;
 
+app.use(logger);
 app.use(express.json());
+app.use(cookieParser);
 app.use("/", express.static(path.join(__dirname, "public")));
 // alternatively: app.use(express.static("public"))
 app.use("/", require("./routes/root"));
@@ -17,4 +22,5 @@ app.all("*", (req, res) => {
         res.type("txt").send("404 Not Found");
     }
 });
+app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
