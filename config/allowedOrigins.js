d3479c1eaa17fe120931acc385a6734f091c45f0
LizzieDworsky <amydworsky15@gmail.com>
2023-12-07 08:37:46 -0700

    added cors options and cookieParser middleware

---
+++ config/allowedOrigins.js
@@ -0,0 +1,5 @@
+const allowedOrigins = [
+    "http://localhost:3000",
+    // any additional urls ex: "http://www.amyrepair.io" and "https://www.amyrepair.io"
+];
+module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const app = express();
 const path = require("path");
 const { logger } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
-//const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
-app.use(cors()); // public access to api
+app.use(cors(corsOptions));
 app.use(express.json());
-//app.use(cookieParser);
+app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "public")));
-// alternatively: app.use(express.static("public"))
 app.use("/", require("./routes/root"));
 app.all("*", (req, res) => {
     res.status(404);
