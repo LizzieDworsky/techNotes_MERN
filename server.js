f2d612456fae7b3ec5647c1230918c3a829313a5
LizzieDworsky <amydworsky15@gmail.com>
2023-12-12 11:05:31 -0700

    added JSDoc to server.js file + removed default value for counter seq field

--- server.js
+++ server.js
@@ -12,15 +12,45 @@ const mongoose = require("mongoose");
 const initializeCounter = require("./config/initDB");
 const PORT = process.env.PORT || 3500;
 
+/**
+ * Connect to the database.
+ */
 connectDB();
+/**
+ * Middleware for logging request details.
+ */
 app.use(logger);
+/**
+ * Enable Cross-Origin Resource Sharing (CORS) with predefined options.
+ */
 app.use(cors(corsOptions));
+/**
+ * Middleware to parse JSON request bodies.
+ */
 app.use(express.json());
+/**
+ * Middleware to parse cookies attached to the client request object.
+ */
 app.use(cookieParser());
+/**
+ * Serve static files located in the 'public' directory.
+ */
 app.use("/", express.static(path.join(__dirname, "public")));
+/**
+ * Root route.
+ */
 app.use("/", require("./routes/root"));
+/**
+ * User routes.
+ */
 app.use("/users", require("./routes/userRoutes"));
+/**
+ * Note routes.
+ */
 app.use("/notes", require("./routes/noteRoutes"));
+/**
+ * Catch-all route handler for any requests to an undefined route.
+ */
 app.all("*", (req, res) => {
     res.status(404);
     if (req.accepts("html")) {
@@ -31,7 +61,14 @@ app.all("*", (req, res) => {
         res.type("txt").send("404 Not Found");
     }
 });
+/**
+ * Global error handler middleware.
+ */
 app.use(errorHandler);
+/**
+ * Event listener for the 'open' event on the mongoose connection.
+ * Starts the server once the database connection is established.
+ */
 mongoose.connection.once("open", async () => {
     console.log("Connected to MongoDB");
     try {
@@ -42,6 +79,10 @@ mongoose.connection.once("open", async () => {
     }
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
+/**
+ * Event listener for the 'error' event on the mongoose connection.
+ * Logs the database connection errors.
+ */
 mongoose.connection.on("error", (err) => {
     console.log(err);
     logEvents(
