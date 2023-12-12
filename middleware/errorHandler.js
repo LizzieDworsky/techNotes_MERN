cb57db1da808a0dbea4a5b0aa4f94c808987da08
LizzieDworsky <amydworsky15@gmail.com>
2023-12-12 11:37:07 -0700

    added JSDoc to errorHandler and logger middleware

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,18 @@
+/**
+ * @file errorHandler.js
+ * Custom error handling middleware for the application.
+ * It logs the error details and sends a response with the error message.
+ */
+
 const { logEvents } = require("./logger");
 
+/**
+ * Error handling middleware.
+ * @param {Object} err - The error object.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {function} next - The next middleware function in the stack.
+ */
 const errorHandler = (err, req, res, next) => {
     logEvents(
         `${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
--- middleware/logger.js
+++ middleware/logger.js
@@ -1,9 +1,20 @@
+/**
+ * @file logger.js
+ * Custom logging middleware and functions for the application.
+ * It logs request and general events to log files.
+ */
+
 const { format } = require("date-fns");
 const { v4: uuid } = require("uuid");
 const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
 
+/**
+ * Logs events to a specified log file.
+ * @param {string} message - The message to log.
+ * @param {string} logFileName - The file name of the log.
+ */
 const logEvents = async (message, logFileName) => {
     const dateTime = format(new Date(), "yyyyMMdd\tHH:mm:ss");
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
@@ -19,9 +30,17 @@ const logEvents = async (message, logFileName) => {
         console.log("Error in logEvents", error);
     }
 };
+
+/**
+ * Middleware that logs each request to a log file.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {function} next - The next middleware function in the stack.
+ */
 const logger = (req, res, next) => {
     logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, "reqLog.log");
     console.log(`${req.method} ${req.path}`);
     next();
 };
+
 module.exports = { logEvents, logger };
